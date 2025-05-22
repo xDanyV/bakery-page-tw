a4176d2442cfaf09abccaa8f02a51c6c7868ad00
xDanyV <danyvalencia0505@hotmail.com>
2025-05-22 08:31:32 -0700

    Mejora en la pagina productos

---
+++ src/data/products.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        id: 1,
+        name: "Tarta de Fresas",
+        category: "Tartas",
+        image: "/images/products/tarta_7.jpg",
+        description: "Deliciosa tarta con fresas naturales.",
+    },
+    {
+        id: 2,
+        name: "Tarta de Vainilla",
+        category: "Tartas",
+        image: "/images/products/tarta_12.jpg",
+        description: "Tarta suave con glaseado de vainilla.",
+    },
+    {
+        id: 3,
+        name: "Cupcake de Chocolate",
+        category: "Cupcake",
+        image: "/images/products/cupcake_1.jpg",
+        description: "Brownie húmedo de chocolate puro.",
+    },
+    {
+        id: 4,
+        name: "Galleta de chocolate",
+        category: "Galleta",
+        image: "/images/products/galleta_1.jpg",
+        description: "Galleta con chispas de chocolate puro.",
+
+    }
+
+];
+
+export default products;
